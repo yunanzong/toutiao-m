297c6ca83697e78950e5cd57a3800db1f2c41637
yunanzong <2219878745@qq.com>
2021-11-01 11:09:42 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
     <router-view/>
     <!-- router-view不能删除，是路由的出口 -->
     <h1>头条</h1>
+    <div>
+      <!-- i标签是斜体文本，意思是原来文本不同的文本，以示区别 -->
+      <i class='toutiao toutiao-shipin'></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-/* 加载全局样式 */
+/* 这是一个移动端的项目，需要把使用一个移动端的组件vant，这里引入vant */
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import './styles/index.less'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Vant).mount('#app')
